a567db405de72a9667b1b120aba08118e1d954e3
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2022-08-20 19:01:45 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function Clicar(){
-    var texto = window.document.querySelector('input#txt');
-    var resultado = window.document.querySelector('div#resultado');
-    var conteudo = String(txt.value);
-    resultado.innerHTML = `Você digitou: ${conteudo}`
-}
