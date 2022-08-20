1e3db949ca916011ecb91c5152854d931e81e9cf
Lucas Gabriel <37782940+v0ltmx@users.noreply.github.com>
2022-08-20 19:02:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function Clicar(){
+
+    document.getElementById("conteudo").innerHTML = "Mordekaiser<br>Bel Veth<br>Lissandra<br>Sivir<br>Renata<br><br>Poppy<br>Viego<br>Sylas<br>Kogmaw<br>Morgana<br><br>Shen<br>Zac<br>Azir<br>Kogmaw<br>Rell";
+        
+}
